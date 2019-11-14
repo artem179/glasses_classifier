09f11fd4d5bcee5eea5186bbd3647a2879b2b756
rizhik179 <artyom179@mail.ru>
2019-11-14 22:03:49 +0300

    Add transformation classes for 255. rgb images without scaling them to [0., 1.]

---
+++ utils/transformation.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+
+
+class ToTensorUint(object):
+    def __call__(self, pic):
+        if isinstance(pic, np.ndarray):
+            img = torch.from_numpy(pic.transpose((2, 0, 1)))
+            return img.float()
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class NormalizeUint(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        tensor.sub_(self.mean).div_(self.std)
+        return tensor
