3493d161c0c37577384396b6dab87da1b49dc8c0
rizhik179 <artyom179@mail.ru>
2019-11-14 22:26:53 +0300

    Added template for detection class which will localize face.

---
+++ detection.py
@@ -0,0 +1,20 @@
+import torch
+import dlib
+import cv2
+import torchvision.transforms as transforms
+from utils.utils import get_roi, crop_img, get_final_roi
+from utils.transformation import ToTensorUint, NormalizeUint
+
+
+class Detector(object):
+    def __init__(self, cnn=False, with_landmarks=True):
+        if cnn:
+            self.face_detector = dlib.cnn_face_detection_model_v1('model_params/mmod_human_face_detector.dat')
+        else:
+            self.face_detector = dlib.get_frontal_face_detector()
+        if with_landmarks:
+            self.transform = transforms.Compose([ToTensorUint(), NormalizeUint(mean=127.5, std=128)])
+            checkpoint = torch.load('model_params/phase1_wpdc_vdc.pth.tar',
+                                    map_location=lambda storage, loc: storage)['state_dict']
+
+            #self.model =
